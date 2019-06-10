65a450edb0d30e53bfe42d02459caf34304bc740
Hannah Nguyen - Learn <hangnm81@gmail.com>
2019-06-10 16:08:29 -0700

    commit server and client code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { API, graphqlOperation } from 'aws-amplify';
+
+const query = `
+  query {
+    listRestaurants {
+      items {
+        id name description location
+      }
+    }
+  }
+`
 
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
+  useEffect(() => {
+    const fetchData = async ()  => {
+    const data = await API.graphql(graphqlOperation(query));
+    setRestaurants(data.data.listRestaurants.items)
+    }
+    fetchData();
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p>        
       </header>
+      <body>
+        {restaurants.map((restaurant, index) => 
+            <div key={index}>{restaurant.name}</div>
+        )}
+      </body>
+
     </div>
   );
 }
